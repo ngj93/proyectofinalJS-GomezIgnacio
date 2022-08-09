dfb0c8fab8b4d59ea9253c6024a09ec5ae6ef695
ngj93 <101230386+ngj93@users.noreply.github.com>
2022-08-09 12:31:08 -0300

    Add files via upload

--- arreglo.js
+++ arreglo.js
@@ -1,25 +1,41 @@
 const arreglo = [
-    { name:'Dolar', abrev:'USD'},
-    { name:'Euro', abrev:'EUR'},
-    { name:'Pesos', abrev:'ARS'},
-    { name:'Peso mexicano', abrev:'MXN'},
-    { name:'Moneda Brasilera', abrev:'BRL'},
-    { name:'Moneda Egipcia', abrev:'EGP'},
-    { name:'Dolar Bahameno', abrev:'BSD'},
-    { name:'Dolar australiano', abrev:'AUD'}
+  { name: "Dolar", abrev: "USD" },
+  { name: "Euro", abrev: "EUR" },
+  { name: "Pesos", abrev: "ARS" },
+  { name: "Peso mexicano", abrev: "MXN" },
+  { name: "Moneda Brasilera", abrev: "BRL" },
+  { name: "Moneda Egipcia", abrev: "EGP" },
+  { name: "Dolar Bahameno", abrev: "BSD" },
+  { name: "Dolar australiano", abrev: "AUD" },
 ];
-arreglo.sort(function(a, b) {
-    if (a.name > b.name) {
-        return -1;
-    }
-    if (a.name < b.name) {
-        return -1;
-    }
-    // a must be equal to b 
-    return 0;
 
+//ORDENO ARRAY
+arreglo.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  return 0;
 });
 
+// IMPRIMO EN HTML
+const abrev = document.getElementById("abrev");
+const disponibles = document.createElement("table");
+arreglo.forEach((element) => {
+  fetch(`https://api.exchangerate-api.com/v4/latest/USD`)
+    .then((res) => res.json())
+    .then((data) => {
+      const tasa = data.rates[element.abrev];
 
-
-
+      disponibles.innerHTML += `
+      <tr>
+      <td>${element.abrev}</td>
+      <td>${element.name}</td>
+      <td>U$D ${tasa}</td>
+      
+      </tr>`;
+    });
+});
+abrev.append(disponibles);
--- theme.js
+++ theme.js
@@ -1,50 +1,43 @@
-let tema = document.querySelector("#tema")
+let tema = document.querySelector("#tema");
 
-tema.addEventListener("click", function(){
-    if (localStorage.getItem("theme") == "dark"){
-        lightMode()
-    }else{
-        darkMode()
-    }
+tema.addEventListener("click", function () {
+  if (localStorage.getItem("theme") == "dark") {
+    lightMode();
     Toastify({
-        text: "Dark Mode-activado",
-        duration: 3000
+      text: "light Mode-activado",
+      duration: 3000,
     }).showToast();
-})
+  } else {
+    darkMode();
+    Toastify({
+      text: "Dark Mode-activado",
+      duration: 3000,
+    }).showToast();
+  }
+});
 
 function darkMode() {
-    let body = document.querySelector("body")
-    body.style.backgroundColor = "black"
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+  tema.innerText = "LightMode";
 
-    localStorage.setItem("theme", "dark")
-    Toastify({
-        text: "Dark Mode-activado",
-        duration: 3000
-    }).showToast();
+  localStorage.setItem("theme", "dark");
 }
 
 function lightMode() {
-    let body = document.querySelector("body")
-    body.style.backgroundColor = "white"
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+  tema.innerText = "darkMode";
 
-    localStorage.setItem("theme", "light")
-    Toastify({
-        text: "Dark Mode-activado",
-        duration: 3000
-    }).showToast();
+  localStorage.setItem("theme", "light");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (localStorage.getItem("theme") == "dark") {
-        darkMode()
-    }else {
-        lightMode()
-    }
-})
-
-// tema.addEventListener('click', () => {
-//     Toastify({
-//         text: "Dark Mode-activado",
-//         duration: 3000
-//     }).showToast();
-// })
+  if (localStorage.getItem("theme") == "dark") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
